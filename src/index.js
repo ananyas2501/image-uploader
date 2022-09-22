80267f7081fcca03517c2d195370f73bb392d7f4
Ananya Shukla <40815040+ananyas2501@users.noreply.github.com>
2022-09-22 21:26:59 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const { BlockBlobClient, AnonymousCredential } = require("@azure/storage-blob");
+
+blobUpload = function(file, url, container, sasKey) {
+    var blobName = buildBlobName(file);
+    var login = `${url}/${container}/${blobName}?${sasKey}`;
+    var blockBlobClient = new BlockBlobClient(login, new AnonymousCredential());
+    const blobOptions = {
+        blobHTTPHeaders: { ContentType: "image/png" },
+    };
+    // var blobHttpHeader = new blobHTTPHeaders (ContentType = "image/jpeg" );
+ 
+    var uploadedBlob = blockBlobClient.UploadAsync(file, blobOptions);
+    // // blockBlobClient.uploadBrowserDataToBlockBlob(file, blobOptions)
+    // blobClient.UploadAsync(file, blobOptions);
+    // // blockBlobClient.uploadBrowserData(file, blobOptions);
+    // // console.log(blobOptions);
+    
+}
+
+function buildBlobName(file) {
+    var filename = file.name.substring(0, file.name.lastIndexOf('.'));
+    var ext = file.name.substring(file.name.lastIndexOf('.'));
+    return filename + ext;
+}
